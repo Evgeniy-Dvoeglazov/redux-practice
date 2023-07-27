380a1187c11f2e81b3401265c9e45a721e0070a6
Evgeniy Dvoeglazov <jindv@yandex.ru>
2023-07-27 14:02:39 +0300

    добавить фунционал увеличения и уменьшения числа при нажатии на кнопки

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
 
 function App() {
+
+  const dispatch = useDispatch();
+  const cash = useSelector(state => state.cash);
+
+  function addCash(cash) {
+    dispatch({type: "ADD_CASH", payload: cash})
+  }  
+  
+  function getCash(cash) {
+    dispatch({type: "GET_CASH", payload: cash})
+  }
+
   return (
     <div className="App">
-      
+      <div>{cash}</div>
+      <div>
+        <button onClick={() => addCash(Number(prompt()))}>Пополнить счёт</button>
+        <button onClick={() => getCash(Number(prompt()))}>Снять со счёта</button>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,14 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { createStore } from 'redux';
+import { legacy_createStore as createStore } from 'redux';
+import { Provider } from 'react-redux';
 
 const defaultState = {
   cash: 0,
 }
 
-const reducer = (state, action) => {
+const reducer = (state = defaultState, action) => {
   switch (action.type) {
+    case "ADD_CASH":
+      return { ...state, cash: state.cash + action.payload }
+
+    case "GET_CASH":
+      return { ...state, cash: state.cash - action.payload }
 
     default:
       return state;
@@ -21,7 +27,9 @@ const store = createStore(reducer)
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
